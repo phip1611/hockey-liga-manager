78c1c556b3a52202c8074298ed35cb9770a8dbd0
Philipp Schuster <phip1611@gmail.com>
2019-10-03 23:00:53 +0200

    Spielbericht-Form: UX (nur die Spieler zur Auswahl, die im Team sind)

--- src/main/java/de/phip1611/hockeyligamanager/controller/TeamRestController.java
+++ src/main/java/de/phip1611/hockeyligamanager/controller/TeamRestController.java
@@ -10,10 +10,12 @@ package de.phip1611.hockeyligamanager.controller;
 import de.phip1611.hockeyligamanager.service.api.TeamService;
 import de.phip1611.hockeyligamanager.service.api.dto.TeamDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("team")
@@ -25,9 +27,14 @@ public class TeamRestController {
         this.service = service;
     }
 
-    @GetMapping("all")
+    @GetMapping()
     public List<TeamDto> getAllTeams() {
         return this.service.findAll();
+    }
+
+    @GetMapping("{uuid}")
+    public TeamDto getTeam(@PathVariable("uuid") UUID uuid) {
+        return this.service.findById(uuid);
     }
 
 }
--- src/main/resources/static/res/js/main.js
+++ src/main/resources/static/res/js/main.js
@@ -1,4 +1,99 @@
-$("#quality-range").on('change input', function() {
-    let value = $(this).val();
-    $("#input-quality-visualizer").html(value);
-});
+const HEIM_SELECTIONS_SELECTOR = '.heim-spieler-selection';
+const GAST_SELECTIONS_SELECTOR = '.gast-spieler-selection';
+const HEIM_TRIGGER_SELECTOR = '#heimteam';
+const GAST_TRIGGER_SELECTOR = '#gastteam';
+
+const REST_URL = '/team/{uuid}';
+
+const d = document;
+const $  = d.querySelector.bind(d);
+const $$ = d.querySelectorAll.bind(d);
+
+window.onload = () => {
+    console.log("js onload");
+    registerEventListeners();
+    filterOptionsOnInit();
+};
+
+function registerEventListeners() {
+    $(HEIM_TRIGGER_SELECTOR).addEventListener('change', (event) => {
+        const uuid = event.target.value;
+        console.log("Heimteam changed: ", event);
+        getTeamByUuid('heim', uuid, processTeamDto);
+    });
+    $(GAST_TRIGGER_SELECTOR).addEventListener('change', (event) => {
+        const uuid = event.target.value;
+        console.log("Gastteam changed: ", event);
+        getTeamByUuid('gast', uuid, processTeamDto);
+    });
+}
+
+/**
+ * Holt ein Team-Dto vom Server anhand seiner UUID um die Spieler des Teams zu finden.
+ *
+ * @param team 'heim' oder 'gast'
+ * @param uuid UUID
+ * @param callback Callback, an das das Team und das Dto gereicht werden
+ */
+function getTeamByUuid(team, uuid, callback) {
+    fetch(REST_URL.replace('{uuid}', uuid))
+        .then(res => res.json())
+        .catch(err => console.error('Konnte Spieler nicht abrufen', err))
+        .then(dto => callback(team, dto))
+        .catch(err => console.error('Fehler beim Verarbeiten der Spieler', err));
+}
+
+function processTeamDto(team, dto) {
+    const selects = team === 'heim' ? $$(HEIM_SELECTIONS_SELECTOR) : $$(GAST_SELECTIONS_SELECTOR);
+    let optionsHtml = '<option value="">-</option>'; // default option
+    dto.spieler.forEach(spieler => {
+       optionsHtml += `<option value="${spieler.id}">${spieler.fullName}</option>`;
+    });
+    selects.forEach(select => {
+       select.innerHTML = optionsHtml;
+    });
+}
+
+/**
+ * Wenn die Seite "initiert" wird, müssen für das ausgewählte Team die Optionen die bereits ausgewählt sind
+ * erhalten bleiben, alle anderen aber gelöscht.
+ */
+function filterOptionsOnInit() {
+    const preselectedHomeValue = $(HEIM_TRIGGER_SELECTOR).value;
+    const preselectedGastValue = $(GAST_TRIGGER_SELECTOR).value;
+
+    if (preselectedHomeValue) {
+        getTeamByUuid('heim', preselectedHomeValue, filterOptions);
+    }
+
+    if (preselectedGastValue) {
+        getTeamByUuid('gast', preselectedGastValue, filterOptions);
+
+    }
+}
+
+/**
+ * Beim initialisieren der Seite werden nun für ein Team alle Options der entsprechenden Selects gefiltert:
+ * alle die nicht dem Team angehören (standardmäßig sind alle in der Auswahl vom Server) werden heraus gefiltert:
+ * Machen kein einfaches ersetzen, da wir options die "selected" sind behalten wollen
+ * @param team
+ * @param dto
+ */
+function filterOptions(team, dto) {
+    const selects = team === 'heim' ? $$(HEIM_SELECTIONS_SELECTOR) : $$(GAST_SELECTIONS_SELECTOR);
+    const spielerIds = dto.spieler.map(spieler => spieler.id); // Array von ID
+    selects.forEach(select => {
+        const options = select.querySelectorAll('option'); // Alle Options einer bestimmten Select-Box
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].value === '') continue; // das leere option feld auch erlauben
+            if (!spielerIds.includes(options[i].value)) {
+                console.log(`Spieler ${options[i].value} ist NICHT in dem Team; fliegt aus der NodeList`);
+                select.removeChild(options[i]);
+            } else {
+                console.log(`Spieler ${options[i].value} ist in dem Team; bleibt in der NodeList`);
+                // vom Server steht im InnerHTML das Proeprty "fullNameWithTeamPrefix"; das können wir hier ersetzen
+                options[i].innerHTML = dto.spieler.find(spieler => spieler.id === options[i].value).fullName;
+            }
+        }
+    });
+}
