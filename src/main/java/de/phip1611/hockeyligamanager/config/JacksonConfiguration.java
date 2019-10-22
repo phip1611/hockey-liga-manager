f7194acd23ee3da28f32e8b7ca84b4e0967b9d05
Philipp Schuster <phip1611@gmail.com>
2019-10-22 22:05:15 +0200

    Alte Export-Dateien importieren (mit invalidem second assist field)

--- src/main/java/de/phip1611/hockeyligamanager/config/JacksonConfiguration.java
+++ src/main/java/de/phip1611/hockeyligamanager/config/JacksonConfiguration.java
@@ -7,6 +7,7 @@
  */
 package de.phip1611.hockeyligamanager.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -23,7 +24,8 @@ public class JacksonConfiguration {
         return new ObjectMapper()
                 .registerModule(new Jdk8Module())
                 .registerModule(new JavaTimeModule()) // um local date time zu (de)serialisieren
-                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
 }
